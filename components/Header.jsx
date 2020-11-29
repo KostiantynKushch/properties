97fe046d00c80d1a511c1e83b6dc68eae6d78684
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-11-29 21:35:53 +0200

    Mobile Menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,102 @@
 import { useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
 import { HEADER_SETTINGS } from '../lib/Queries';
 import { initializeApollo, addApolloState } from '../lib/apolloClient';
 import Link from 'next/link';
 import ActiveLink from './ActiveLink';
 import { Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
+import { Sling as Hamburger } from 'hamburger-react';
+
+function debounce(fn, ms) {
+  let timer;
+  return (_) => {
+    clearTimeout(timer);
+    timer = setTimeout((_) => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
 
 const Header = () => {
+  const [dimensions, setDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
+  const [isOpenHamb, setOpenHamb] = useState(false);
+
+  const debouncedHandleResize = debounce(() => {
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+  }, 300);
+
+  useEffect(() => {
+    debouncedHandleResize();
+
+    window.addEventListener('resize', debouncedHandleResize);
+
+    return (_) => {
+      window.removeEventListener('resize', debouncedHandleResize);
+    };
+  }, []);
+
   const { loading, error, data } = useQuery(HEADER_SETTINGS);
   if (loading) return <p>Loader ...</p>;
   if (error) return <p>Error :</p>;
 
   const menuItems = data.menus.nodes[0].menuItems.nodes;
-
   return (
-    <HeaderContainer>
-      <SRow>
-        <Col>
-          <Link href="/">
-            <a>
-              <img src="/logo_dark.png" alt="" />
-            </a>
-          </Link>
-        </Col>
-        <Col>
-          <Nav>
-            <MenuList>
+    <>
+      <HeaderContainer>
+        <SRow>
+          <LogoCol>
+            <Link href="/">
+              <a>
+                <img src="/logo_dark.png" alt="" />
+              </a>
+            </Link>
+          </LogoCol>
+          <Col>
+            <Nav
+              style={
+                dimensions.width <= 768
+                  ? { justifyContent: 'flex-end', marginRight: '-30px' }
+                  : {}
+              }
+            >
+              {dimensions.width > 768 && (
+                <MenuList>
+                  {menuItems.map((item) => (
+                    <MenuItem key={item.id}>
+                      <ActiveLink
+                        activeClassName="active-link"
+                        href={item.path}
+                      >
+                        <a>{item.label}</a>
+                      </ActiveLink>
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              )}
+
+              {dimensions.width <= 768 && (
+                <Hamburger
+                  toggled={isOpenHamb}
+                  toggle={setOpenHamb}
+                  color="#77838f"
+                />
+              )}
+            </Nav>
+          </Col>
+        </SRow>
+      </HeaderContainer>
+      {dimensions.width <= 768 && (
+        <MobileMenu>
+          <MobileMenuContainer className={isOpenHamb ? 'show' : null}>
+            <MobileMenuList>
               {menuItems.map((item) => (
                 <MenuItem key={item.id}>
                   <ActiveLink activeClassName="active-link" href={item.path}>
@@ -33,11 +104,11 @@ const Header = () => {
                   </ActiveLink>
                 </MenuItem>
               ))}
-            </MenuList>
-          </Nav>
-        </Col>
-      </SRow>
-    </HeaderContainer>
+            </MobileMenuList>
+          </MobileMenuContainer>
+        </MobileMenu>
+      )}
+    </>
   );
 };
 
@@ -60,6 +131,29 @@ const Nav = styled.nav`
   align-items: center;
 `;
 
+const LogoCol = styled(Col)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const MobileMenu = styled.nav`
+  position: relative;
+  .show {
+    background: #fff;
+    top: 0;
+  }
+`;
+
+const MobileMenuContainer = styled.div`
+  position: absolute;
+  top: -100vh;
+  left: 0;
+  height: calc(100vh - 80px);
+  width: 100%;
+  transition: all 0.3s ease;
+`;
+
 const MenuList = styled.ul`
   width: 100%;
   display: flex;
@@ -67,6 +161,12 @@ const MenuList = styled.ul`
   justify-content: space-between;
   padding-left: 0;
   margin-bottom: 0;
+`;
+
+const MobileMenuList = styled(MenuList)`
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
 `;
 
 const MenuItem = styled.li`
